77472b422154198b7a9d0727438c1393545f7f9a
Brian Cheung <bcheung.dev@gmail.com>
2025-11-03 02:27:47 -0600

    feat(docs): deploy to custom domain

--- alchemy.run.ts
+++ alchemy.run.ts
@@ -3,17 +3,25 @@ import { Website } from "alchemy/cloudflare";
 
 const app = await alchemy("useflow");
 
+const domain =
+  app.stage === "production" ? "useflow.sh" : `${app.stage}.useflow.sh`;
+
+const docsDomain = `docs.${domain}`;
 const docs = await Website("docs", {
   assets: "dist",
   cwd: "./apps/docs",
   build: "bun run build",
+  bindings: {
+    DOCS_URL: `https://${docsDomain}`,
+  },
   dev: {
     command: "bun run dev",
   },
+  domains: [docsDomain],
 });
 
 console.log({
-  docs: docs.url,
+  docs: docs.domains?.map((domain) => domain.name),
 });
 
 await app.finalize();
